9202626df25392e606372e9ead3cda2fa9da47db
sosokereselidze <soso.kereselidze1@gmail.com>
2025-01-26 20:11:58 +0400

    commit

--- day 0/1/index.js
+++ day 0/1/index.js
@@ -1,15 +1,14 @@
 function countMultiples(a, b, c) {
-    if (a % c === 0 || b % c === 0) {
-      return 'infinite';
-    }
-  
-    const numMultiples = Math.floor(b / c) - Math.ceil(a / c) + 1;
-  
-    return numMultiples;
+  if (a % c === 0 || b % c === 0) {
+    return 'infinite';
   }
   
-  const a = 1;
-  const b = 10;
-  const c = 3;
-  const result = countMultiples(a, b, c);
-  console.log(`Number of multiples of ${c} between ${a} and ${b}: ${result}`);
+  const numMultiples = Math.floor(b / c) - Math.ceil(a / c) + 1; 
+  return numMultiples;
+}
+
+const a = 1;
+const b = 20;
+const c = 3;
+const result = countMultiples(a, b, c);
+console.log(`Number of multiples of ${c} between ${a} and ${b}: ${result}`);
